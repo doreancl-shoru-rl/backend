a890b252d020f7306768e89713fbbb480e060e47
doreancl <doreancl@gmail.com>
2022-03-27 12:32:26 -0300

    change hard values with environment parameters

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,25 @@ import { AppService } from './app.service';
 import { LinksModule } from './links/links.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { HolaModule } from './hola/hola.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'REMOVED',
-    ),
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const username = configService.get('MONGO_USERNAME');
+        const password = configService.get('MONGO_PASSWORD');
+        const database = configService.get('MONGO_DATABASE');
+        const host = configService.get('MONGO_HOST');
+        return {
+          uri: `mongodb+srv://${username}:${password}@${host}?retryWrites=true&w=majority`,
+          dbName: database,
+        };
+      },
+      inject: [ConfigService],
+    }),
     LinksModule,
     HolaModule,
   ],
